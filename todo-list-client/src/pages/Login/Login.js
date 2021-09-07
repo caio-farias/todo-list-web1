6b58e8c56fa82ac4fb35f460c36ec12567303467
Caio Farias <caio4095@gmail.com>
2021-09-07 13:56:45 -0300

    New register and login flux; New dashboard style;

--- todo-list-client/src/pages/Login/Login.js
+++ todo-list-client/src/pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button } from '../../components'
+import { Button, Loading } from '../../components'
 import api from '../../api'
 import './index.scss'
 import { setAuthToken, setUserId } from '../../utils'
@@ -9,10 +9,20 @@ const Login = () => {
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ errorMessage, setErrorMessage] = useState('')
+  const [ isLoading, setIsLoading ] = useState(false)
   const history = useHistory()
+
+  const changePage = (status) =>{
+    setTimeout(() => {
+      history.push({
+        pathname: '/app'
+      })
+    }, 500);
+  }
 
   const submit = async e =>{
     e.preventDefault()
+    setIsLoading(true)
     try {
       const res = await api.post('/auth', {
         email,
@@ -22,10 +32,9 @@ const Login = () => {
       const { _id, } = user
       setUserId(_id)
       setAuthToken(token)
-      history.push({
-        pathname: '/app'
-      })
+      changePage()
     } catch (error) {
+      setIsLoading(false)
       setErrorMessage("Ocorreu um erro")
     }
   }
@@ -34,40 +43,58 @@ const Login = () => {
     <section className="login-page">
       <div className="login-user-form-wrapper">
         <span className="error-message"></span>
-        <form className="login-user-form" onSubmit={submit}>
-          <div className="user-form-field">
-            <label className="user-form-label" htmlFor="email">E-mail: </label>
-            <input 
-              required 
-              className="user-form-input" 
-              type="text" 
-              name="email"
-              onChange={ e => setEmail(e.target.value) }
-            />
-          </div>
-          <div className="user-form-field">
-            <label className="user-form-label" htmlFor="password">Senha: </label>
-            <input 
-              required 
-              className="user-form-input" 
-              type="password" 
-              name="password"
-              onChange={ e => setPassword(e.target.value) }
-            />
-          </div>
-          {errorMessage && ( 
-            <span className="error-message">
-              {errorMessage}
-            </span> 
-          )}
-          <div className="user-form-button-wrapper">
-            <Button type='submit' modifier='login'>Entrar</Button>
-          </div>
-        </form>
+        {isLoading ? 
+          <div className="loading-wrapper">
+            <span> Por favor, aguarde.. </span>
+            <Loading type='spin' color='#1fb092' />
+          </div> 
+          :
+          <LoginForm 
+            setEmail={setEmail}
+            setPassword={setPassword}
+            submit={submit} 
+            errorMessage={errorMessage}
+          />}
       </div>
     </section>
     </>
   )
 }
+
+const LoginForm = ({ setPassword, setEmail, submit, errorMessage}) => (
+  <>
+  <form className="login-user-form" onSubmit={submit}>
+    <div className="user-form-field">
+      <label className="user-form-label" htmlFor="email">E-mail: </label>
+      <input 
+        required 
+        className="user-form-input" 
+        type="text" 
+        name="email"
+        onChange={ e => setEmail(e.target.value) }
+      />
+    </div>
+    <div className="user-form-field">
+      <label className="user-form-label" htmlFor="password">Senha: </label>
+      <input 
+        required 
+        className="user-form-input" 
+        type="password" 
+        name="password"
+        onChange={ e => setPassword(e.target.value) }
+      />
+    </div>
+    {errorMessage && ( 
+      <span className="error-message">
+        {errorMessage}
+      </span> 
+    )}
+    <div className="user-form-button-wrapper">
+      <Button type='submit' modifier='login'>Entrar</Button>
+    </div>
+  </form>
+  </>
+)
+
 
 export default Login
--- todo-list-client/src/pages/Register/Register.js
+++ todo-list-client/src/pages/Register/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { useHistory } from "react-router"
-import { Button } from "../../components"
+import { Button, Loading } from "../../components"
 import api from '../../api'
 import './index.scss'
 
@@ -9,74 +9,97 @@ const Register = () => {
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ errorMessage, setErrorMessage] = useState('')
+  const [ isLoading, setIsLoading ] = useState(false)
   const history = useHistory()
+
+  const changePage = (status) =>{
+    setTimeout(() => {
+      history.push({
+        pathname: '/login'
+      })
+    }, 500);
+  }
 
   const submit = async e =>{
     e.preventDefault()
-    console.log(name)
+    setIsLoading(true)
     try {
       await api.post('/users', {
         name,
         email,
         password,
       })
-      history.push({
-        pathname: '/login'
-      })
+      changePage()
     } catch (error) {
-      setErrorMessage("Ocorreu um erro")
+      setIsLoading(false)
+      setErrorMessage("Ocorreu um erro, tente novamente.")
     }
   }
+
 
   return (
     <>
     <section className="register-page">
-    <div className="register-form-wrapper">
-      <form className="register-form" onSubmit={submit}>
-        <div className="register-form-field">
-          <label className="register-form-label" htmlFor="name">Nome: </label>
-          <input 
-            required 
-            className="register-form-input" 
-            type="text" 
-            name="name"
-            onChange={ e => setName(e.target.value)}
-            />
-        </div>
-        <div className="register-form-field">
-          <label className="register-form-label" htmlFor="password">Senha: </label>
-          <input 
-            required 
-            className="register-form-input" 
-            type="password" 
-            name="password" 
-            onChange={ e => setPassword(e.target.value)}
-            />
-        </div>
-        <div className="register-form-field">
-          <label className="register-form-label" htmlFor="email">E-mail: </label>
-          <input 
-            required 
-            className="register-form-input" 
-            type="text" 
-            name="email" 
-            onChange={ e => setEmail(e.target.value)}
-            />
-        </div>
-        {errorMessage && ( 
-            <span className="error-message">
-              {errorMessage}
-            </span> 
-          )}
-        <div className="register-form-button-wrapper">
-          <a href="/login">Cancelar</a>
-          <Button type='submit' modifier='register'>Cadastrar</Button>
-        </div>
-      </form>
-    </div>
+      <div className="register-form-wrapper">
+        {isLoading ? 
+          <div className="loading-wrapper">
+            <span> Por favor, aguarde.. </span>
+            <Loading type='spin' color='#1fb092' />
+          </div> 
+          :
+          <RegisterForm 
+            setName={setName}
+            setEmail={setEmail}
+            setPassword={setPassword}
+            submit={submit} 
+            errorMessage={errorMessage}
+          />}
+      </div>
     </section>
     </>
   )
 }
+
+const RegisterForm = ({ setName, setPassword, setEmail, submit, errorMessage }) => (
+  <form className="register-form" onSubmit={submit}>
+    <div className="register-form-field">
+      <label className="register-form-label" htmlFor="name">Nome: </label>
+      <input
+        required
+        className="register-form-input"
+        type="text"
+        name="name"
+        onChange={e => setName(e.target.value)} />
+    </div>
+    <div className="register-form-field">
+      <label className="register-form-label" htmlFor="password">Senha: </label>
+      <input
+        required
+        className="register-form-input"
+        type="password"
+        name="password"
+        onChange={e => setPassword(e.target.value)} />
+    </div>
+    <div className="register-form-field">
+      <label className="register-form-label" htmlFor="email">E-mail: </label>
+      <input
+        required
+        className="register-form-input"
+        type="text"
+        name="email"
+        onChange={e => setEmail(e.target.value)} />
+    </div>
+    {errorMessage && (
+      <span className="error-message">
+        {errorMessage}
+      </span>
+    )}
+    <div className="register-form-button-wrapper">
+      <a href="/login">Cancelar</a>
+      <Button type='submit' modifier='register'>Cadastrar</Button>
+    </div>
+  </form>
+)
+
 
 export default Register
